b154a6343384758a641eba1803eb08d48edf5c2b
nuyhv <volumefd@gmail.com>
2023-10-23 18:11:14 +0900

    :lipstick: 기본 width 및 height 설정

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,7 +1,28 @@
 import tw from 'tailwind-styled-components';
+import styled from 'styled-components';
 
-const StyledHeader = tw.header``;
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+  height: 65px;
+  padding: 0.7rem 1rem;
+`;
+
+const Logo = tw.div`
+  w-10
+  items-center
+  text-center
+`;
 
 export const Header = () => {
-  return <StyledHeader>헤더</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logo>로고</Logo>
+      <div>검색바</div>
+      <div>nav 메뉴</div>
+      <div>회원 기능</div>
+    </StyledHeader>
+  );
 };
