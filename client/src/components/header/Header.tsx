b8cc5e92e50f90a44f0c333582c463e724abc167
nuyhv <volumefd@gmail.com>
2023-10-23 18:33:56 +0900

    :lipstick: Header shadowbox 추가

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import tw from 'tailwind-styled-components';
 import styled from 'styled-components';
+import { SearchBar } from './SearchBar';
 
 const StyledHeader = styled.header`
   display: flex;
@@ -7,7 +8,11 @@ const StyledHeader = styled.header`
   gap: 1rem;
   width: 100%;
   height: 65px;
-  padding: 0.7rem 1rem;
+  padding: 0.7rem 1.5rem;
+  box-shadow:
+    0 1px 2px hsla(0, 0%, 0%, 0.05),
+    0 1px 4px hsla(0, 0%, 0%, 0.05),
+    0 2px 8px hsla(0, 0%, 0%, 0.05);
 `;
 
 const Logo = tw.div`
@@ -20,7 +25,7 @@ export const Header = () => {
   return (
     <StyledHeader>
       <Logo>로고</Logo>
-      <div>검색바</div>
+      <SearchBar />
       <div>nav 메뉴</div>
       <div>회원 기능</div>
     </StyledHeader>
