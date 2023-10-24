9cb6b52caa839ce1a1bc3fe5c8b498f3ed6ed64e
Saekyul <saekyulyang@gmail.com>
2023-10-25 01:21:59 +0900

    ✨ TabContainer 디자인

--- client/src/components/openClass/TabContainer.tsx
+++ client/src/components/openClass/TabContainer.tsx
@@ -1,21 +1,52 @@
-import tw from "tailwind-styled-components"
+import tw from 'tailwind-styled-components';
 
 const Container = tw.div`
-    flex
-`
+    flex-col
+
+    p-6
+`;
 
 const Title = tw.div`
     font-bold
 `;
 
-const TabContainer = () => {
-    return (
-        <>
-            <Container>
-                <Title>땡땡땡님의 강좌개설</Title>
-            </Container>
-        </>
-    )
-}
+const Tabs = tw.div`
+    flex
+    gap-4
+    justify-center
 
-export default TabContainer
+    p-4
+`;
+
+const Tab = tw.div`
+    border-solid
+    border-1
+    rounded-xl
+    cursor-pointer
+
+    px-4
+    py-1
+
+    shadow-md
+
+   hover:bg-signature
+   hover:font-bold
+`;
+
+const TabContainer = () => {
+  return (
+    <>
+      <Container>
+        <Title>{'OOO'}님의 강좌개설</Title>
+
+        <Tabs>
+          <Tab>기본정보입력</Tab>
+          <Tab>강좌상세내용</Tab>
+          <Tab>개설정보확인</Tab>
+        </Tabs>
+      </Container>
+    </>
+  );
+};
+
+export default TabContainer;
