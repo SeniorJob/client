01f55abc24aba09ddd74c71699ef81e86bb6c4b2
nuyhv <volumefd@gmail.com>
2023-10-25 15:32:24 +0900

    :sparkles: 메인 페이지 초기구성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Header } from './components/header/Header';
-
+import { MainPage } from './pages/mainpage/MainPage';
 import Login from './components/login/login';
 import OpenClassPage from './components/openClass/OpenClassPage';
 
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage />
       <Login />
       <OpenClassPage />
     </div>
