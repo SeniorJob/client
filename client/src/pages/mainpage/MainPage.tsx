cf70c6992008ab8b8872ad24fd7de947af967054
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-26 23:54:03 +0900

    :sparkles: Category 컴포넌트 연결

--- client/src/pages/mainpage/MainPage.tsx
+++ client/src/pages/mainpage/MainPage.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { ClassRecommend } from '../../components/main/ClassRecommend';
 import { MainSearch } from '../../components/main/MainSearch';
+import { Category } from '../../components/main/Category';
 
 const Main = styled.main`
   min-height: 800px;
@@ -11,6 +12,7 @@ export const MainPage = () => {
     <Main>
       <ClassRecommend />
       <MainSearch />
+      <Category />
     </Main>
   );
 };
