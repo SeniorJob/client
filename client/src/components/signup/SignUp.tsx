f0919b6a8b8513cf00cfdd03dad3be86d81532de
JongHoSke <astra9706@naver.com>
2023-10-27 11:05:42 +0900

    :sparkles:: 회원가입  페이지 레이아웃 수정

--- client/src/components/signup/SignUp.tsx
+++ client/src/components/signup/SignUp.tsx
@@ -1,18 +1,13 @@
 import tw from 'tailwind-styled-components';
 import styled from 'styled-components';
 import SingUpFrom from './SignUpFrom';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import SignSlider from './SignTopSlider';
 const SignUp = () => {
   return (
     <SignLayout>
       <SignDivBox>
         <SignTopText>회원가입</SignTopText>
-        <Swiper slidesPerView={1}>
-          <SwiperSlide>슬라이드 1</SwiperSlide>
-          <SwiperSlide>슬라이드 2</SwiperSlide>
-          <SwiperSlide>슬라이드 3</SwiperSlide>
-        </Swiper>
+        <SignSlider />
         <SingUpFrom />
       </SignDivBox>
     </SignLayout>
