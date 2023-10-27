810a7f183fb68579307e96aeebfae57ab1123ad5
JongHoSke <astra9706@naver.com>
2023-10-27 11:16:34 +0900

    :sparkles: 회원가입 페이지 레이아웃 구현 중_1

--- client/src/components/signup/SignUpFrom.tsx
+++ client/src/components/signup/SignUpFrom.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
+
 import tw from 'tailwind-styled-components';
 import styled from 'styled-components';
 
 const SingUpFrom = () => {
+  const [id, setId] = useState('');
+
   return (
     <SignUpFromLayout>
       <SignUpInputForm>
         <InputWrapper>
+          <InputLabel>이름</InputLabel>
+          <Input type="text" placeholder="예) 홍길동" />
+        </InputWrapper>
+        <InputWrapper>
           <InputLabel>전화번호</InputLabel>
-          <Input type="text" placeholder="01012345678" />
+          <Input type="text" placeholder="예) 01012345678" />
         </InputWrapper>
         <InputWrapper>
           <InputLabel>비밀번호</InputLabel>
@@ -16,6 +24,14 @@ const SingUpFrom = () => {
         <InputWrapper>
           <InputLabel>비밀번호 확인</InputLabel>
           <Input type="password" placeholder="******" />
+        </InputWrapper>
+        <InputWrapper>
+          <InputLabel>직업</InputLabel>
+          <Input type="password" placeholder="******" />
+        </InputWrapper>
+        <InputWrapper>
+          <InputLabel>생년월일</InputLabel>
+          <Input type="password" placeholder="" />
         </InputWrapper>
         <SignUpBtn>가입하기</SignUpBtn>
       </SignUpInputForm>
