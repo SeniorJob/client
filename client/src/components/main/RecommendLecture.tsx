cf8563a018a837edf9a89d7cefc0c7b45574f09c
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-29 01:16:10 +0900

    :sparkles: 강좌 추천 컴포넌트 생성, 더미데이터 렌더링 테스트

--- client/src/components/main/RecommendLecture.tsx
+++ client/src/components/main/RecommendLecture.tsx
@@ -1,7 +1,66 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import { NavButton } from './SwiperNavButton';
+import styled from 'styled-components';
+
+const LectureCard = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Card = styled.div`
+  width: 100%;
+  height: 150px;
+  background-color: lightgreen;
+`;
+
 export const RecommendLecture = () => {
   return (
     <section className="py-6">
-      <div className="container">추천 강좌 리스트</div>
+      <div className="container">
+        <h1>추천 강좌 리스트</h1>
+        <Swiper
+          slidesPerView={5}
+          spaceBetween={10}
+          navigation={{
+            prevEl: '.swiper-lecture-prev',
+            nextEl: '.swiper-lecture-next',
+          }}
+          modules={[Navigation]}
+        >
+          <SwiperSlide>
+            <LectureCard>
+              <Card />
+            </LectureCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <LectureCard>
+              <Card />
+            </LectureCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <LectureCard>
+              <Card />
+            </LectureCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <LectureCard>
+              <Card />
+            </LectureCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <LectureCard>
+              <Card />
+            </LectureCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <LectureCard>
+              <Card />
+            </LectureCard>
+          </SwiperSlide>
+          <NavButton navName="swiper-lecture" />
+        </Swiper>
+      </div>
     </section>
   );
 };
