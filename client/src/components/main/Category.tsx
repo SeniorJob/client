68bd8e11bd231bead4f8e18a80ccc260dc28c40b
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-28 16:42:08 +0900

    :sparkles: Slide custom nav button 연결

--- client/src/components/main/Category.tsx
+++ client/src/components/main/Category.tsx
@@ -1,24 +1,18 @@
 import styled from 'styled-components';
 import { Card } from '../category/CategoryCard';
 import { categoryData } from '../category/categoryData';
+import { CategoryNav } from './SwiperNavButton';
 // Swiper 관련 모듈 Import
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode } from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 const Section = styled.section`
   position: relative;
   max-width: 1200px;
   padding: 1rem;
   margin: 0 auto;
-`;
-
-const CategoryTag = styled(SwiperSlide)`
-  /* background-color: lightgreen; */
-  width: fit-content !important;
-  border-radius: 1rem;
 `;
 
 const CardButton = styled.button`
@@ -37,12 +31,15 @@ export const Category = () => {
           grabCursor={true}
           slidesPerView={10}
           spaceBetween={45}
-          freeMode={true}
-          modules={[FreeMode]}
+          navigation={{
+            prevEl: '.swiper-prev',
+            nextEl: '.swiper-next',
+          }}
+          modules={[Navigation]}
           className="mySwiper"
         >
           {categoryData.map(item => (
-            <CategoryTag>
+            <SwiperSlide>
               <CardButton>
                 <Card>
                   <img src={item.img} alt={item.title} />
@@ -51,10 +48,11 @@ export const Category = () => {
                   <span>{item.title}</span>
                 </div>
               </CardButton>
-            </CategoryTag>
+            </SwiperSlide>
           ))}
         </Swiper>
       </div>
+      <CategoryNav />
     </Section>
   );
 };
