3a81a5609aea0eb60c53be256d753bf54dca863d
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-28 23:54:07 +0900

    :sparkles: 추천 강의 컴포넌트 연결

--- client/src/pages/mainpage/MainPage.tsx
+++ client/src/pages/mainpage/MainPage.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { TopBanner } from '../../components/main/TopBanner';
 import { MainSearch } from '../../components/main/MainSearch';
 import { Category } from '../../components/main/Category';
+import { RecommendLecture } from '../../components/main/RecommendLecture';
 
 const Main = styled.main`
   min-height: 800px;
@@ -13,6 +14,7 @@ export const MainPage = () => {
       <TopBanner />
       <MainSearch />
       <Category />
+      <RecommendLecture />
     </Main>
   );
 };
