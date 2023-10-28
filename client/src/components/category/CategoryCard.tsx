a63c6691a801ec8abc11e5b561d8a123d5a3823f
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-28 13:50:34 +0900

    :sparkles: Category 컴포넌트 및 데이터 셋

--- client/src/components/category/CategoryCard.tsx
+++ client/src/components/category/CategoryCard.tsx
@@ -1,1 +1,22 @@
-export const CategoryCard = () => {};
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  width: 70px;
+  height: 70px;
+  padding: 0.5rem;
+  border-radius: 25%;
+  background-color: #fff;
+  /* border: 1px solid rgba(0, 0, 0, 0.1); */
+  transition: 0.3s ease;
+  text-align: center;
+  overflow: hidden;
+  &:hover {
+    scale: 1.05;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 25%;
+  }
+`;
---
+++ client/src/components/category/categoryData.ts
@@ -0,0 +1,22 @@
+export const categoryData = [
+  { title: '외식', img: 'https://img.icons8.com/stickers/100/food.png' },
+  { title: '서비스', img: 'https://img.icons8.com/stickers/100/trust.png' },
+  {
+    title: '사무직',
+    img: 'https://img.icons8.com/stickers/100/permanent-job.png',
+  },
+  { title: '기술', img: 'https://img.icons8.com/stickers/100/craft-work.png' },
+  {
+    title: '운전',
+    img: 'https://img.icons8.com/stickers/100/steering-wheel.png',
+  },
+  { title: '디자인', img: 'https://img.icons8.com/stickers/100/design.png' },
+  { title: 'IT', img: 'https://img.icons8.com/stickers/100/internet.png' },
+  { title: '생산', img: 'https://img.icons8.com/stickers/100/worker-male.png' },
+  {
+    title: '교육',
+    img: 'https://img.icons8.com/stickers/100/graduation-cap.png',
+  },
+  { title: '의료', img: 'https://img.icons8.com/stickers/100/doctors-bag.png' },
+  { title: '기타', img: 'https://img.icons8.com/stickers/100/more.png' },
+];
