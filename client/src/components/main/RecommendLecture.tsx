de65a2c576cc4aed5cad1d334cada8ffbe0e8f16
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-29 22:28:14 +0900

    :sparkles: 추천 강의 임시 api 연결

--- client/src/components/main/RecommendLecture.tsx
+++ client/src/components/main/RecommendLecture.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import { NavButton } from './SwiperNavButton';
 import styled from 'styled-components';
+import { getLecture } from '../../api/lecture';
 
 const LectureCard = styled.div`
   width: 100%;
@@ -12,9 +14,34 @@ const Card = styled.div`
   width: 100%;
   height: 150px;
   background-color: lightgreen;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
 
+type LectureObject = {
+  [key: string]: string | undefined;
+};
+
 export const RecommendLecture = () => {
+  const [data, setData] = useState<LectureObject[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const lectureData = await getLecture();
+        setData(lectureData);
+        console.log(lectureData);
+      } catch (error) {
+        console.error('에러 발생:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="py-6">
       <div className="container">
@@ -28,36 +55,16 @@ export const RecommendLecture = () => {
           }}
           modules={[Navigation]}
         >
-          <SwiperSlide>
-            <LectureCard>
-              <Card />
-            </LectureCard>
-          </SwiperSlide>
-          <SwiperSlide>
-            <LectureCard>
-              <Card />
-            </LectureCard>
-          </SwiperSlide>
-          <SwiperSlide>
-            <LectureCard>
-              <Card />
-            </LectureCard>
-          </SwiperSlide>
-          <SwiperSlide>
-            <LectureCard>
-              <Card />
-            </LectureCard>
-          </SwiperSlide>
-          <SwiperSlide>
-            <LectureCard>
-              <Card />
-            </LectureCard>
-          </SwiperSlide>
-          <SwiperSlide>
-            <LectureCard>
-              <Card />
-            </LectureCard>
-          </SwiperSlide>
+          {data.map(data => (
+            <SwiperSlide>
+              <LectureCard>
+                <Card>
+                  <img src={data.image_url} alt={data.title} />
+                </Card>
+                <h2>{data.title}</h2>
+              </LectureCard>
+            </SwiperSlide>
+          ))}
           <NavButton navName="swiper-lecture" />
         </Swiper>
       </div>
