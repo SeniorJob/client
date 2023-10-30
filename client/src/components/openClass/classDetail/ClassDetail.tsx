793aa3a1dfafabeb95495f373561f44a697c8b64
Saekyul Yang <saekyulyang@gmail.com>
2023-10-30 23:11:55 +0900

    ✨ 컴포넌트 디자인

--- client/src/components/openClass/classDetail/ClassDetail.tsx
+++ client/src/components/openClass/classDetail/ClassDetail.tsx
@@ -3,19 +3,19 @@ import { OpenButton } from '../OpenButton';
 import { FC, useState } from 'react';
 
 const Container = tw.div`
-    m-4
-    p-4
+  m-4
+  p-4
 
-    bg-signature
+  bg-signature
 `;
 
 const SubTitle = tw.div`
-    text-2xl
-    font-bold
-    pb-4
-    border-b-4
-    border-dotted
-    border-gray-300
+  text-2xl
+  font-bold
+  pb-4
+  border-b-4
+  border-dotted
+  border-gray-300
 `;
 
 interface ClassDetailProps {
@@ -23,26 +23,61 @@ interface ClassDetailProps {
 }
 
 // 주차별 학습내용 컴포넌트 관련 코드
+const WeekClassTitle = tw.div`
+  flex
+  bg-stone-100
+  mt-4
+  p-2
+  text-xl
+  font-bold
+
+  justify-between
+`;
+
+const WeekClassContent = tw.div`
+  px-6
+  py-2
+  bg-white
+  text-lg
+  border-b-2
+  border-solid
+  border-gray-300
+`;
+
+const ActionButton = tw.div`
+  text-lg
+  font-normal
+`;
+
+const AddButton = tw.div`
+  flex
+  p-2
+  text-lg
+  justify-center
+  m-auto
+  bg-white
+`;
+
 interface WeekClassProps {
   week: number;
   classTitle: string;
 }
 
-const WeekClassTitle = tw.div`
-  flex
-  bg-stone-100
-  my-4
-  p-2
-  text-xl
-  font-bold
-`;
-
 const WeekClass: FC<WeekClassProps> = ({ week, classTitle }) => {
   return (
     <div>
       <WeekClassTitle>
-        {week}주차 {classTitle}
+        <div>
+          {week}주차 {classTitle}
+        </div>
+        <div className="flex gap-2">
+          <ActionButton>수정</ActionButton>
+          <ActionButton>삭제</ActionButton>
+        </div>
       </WeekClassTitle>
+      <WeekClassContent>유튜브란 무엇인가</WeekClassContent>
+      <WeekClassContent>구독이란?</WeekClassContent>
+      <AddButton>상세내용 추가하기</AddButton>
     </div>
   );
 };
