ca3d2b080b5e24c25c982c6d84939fd487576de8
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-10-31 13:14:43 +0900

    :sparkles: RecommendType 스크립트 추가

---
+++ client/src/components/main/recommendType.ts
@@ -0,0 +1,10 @@
+export const recommendPopular = {
+  title: '지금 HOT한 강좌들이에요 🔥',
+  subTitle: '인기많은 강좌를 수강해 보세요!',
+  params: 'popular?limit=5',
+};
+
+export const recommendNewest = {
+  title: '따끈따끈한 새 강좌들을 만나보세요! ✨',
+  params: 'filter?filter=latest&descending=true',
+};
