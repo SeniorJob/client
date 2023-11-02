78322f8f3d84ba3e6d6f8427153c8864c9a5369b
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-02 20:58:11 +0900

    :sparkles: 카테고리 필터 레이아웃 구성

--- client/src/components/lecture/LectureFilter.tsx
+++ client/src/components/lecture/LectureFilter.tsx
@@ -6,6 +6,46 @@ import { useLocation, useNavigate } from 'react-router-dom';
 const Filter = styled.div`
   width: 200px;
   border: 1px solid #ccc;
+  border-bottom: none;
+  background-color: #f6f6f6;
+  /* padding: 0.8rem; */
+`;
+
+const CategoryFilter = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  /* gap: 0.5rem; */
+`;
+
+const CategoryLabel = styled.label`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: 0.9rem;
+  height: 22px;
+  flex: 0;
+  gap: 0.4rem;
+  .checked {
+    color: var(--primaryColor);
+    &::before {
+      content: '✓';
+      margin-right: 0.2rem;
+    }
+  }
+`;
+
+const CategoryInput = styled.input`
+  display: none;
+`;
+
+const Category = styled.div`
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.1rem;
+  padding: 1.1rem;
+  width: 100%;
+  border-bottom: 1px solid #ccc;
 `;
 
 export const LectureFilter = () => {
@@ -15,13 +55,20 @@ export const LectureFilter = () => {
   const searchParams = new URLSearchParams(location.search);
   const curCategory = searchParams.get('category');
 
+  const viewAll = () => {
+    searchParams.delete('category');
+    navigate({
+      pathname: '/lectures/filter',
+      search: searchParams.toString(),
+    });
+    console.log(curCategory);
+  };
+
   const handleCategoryFilter = (category: string) => {
     if (curCategory === category) {
-      // If the clicked category is the same as the current one, clear it.
       setCategory('');
       searchParams.delete('category');
     } else {
-      // Set the new category.
       setCategory(category);
       searchParams.set('category', category);
     }
@@ -34,24 +81,36 @@ export const LectureFilter = () => {
   return (
     <aside>
       <Filter>
-        필터
         <div>
-          <div>강좌상태 필터</div>
-          <div>지역 검색</div>
-          <div>
-            카테고리 필터:
-            {categoryData.map(data => (
-              <label key={data.id}>
-                <input
+          <CategoryFilter>
+            <CategoryLabel>
+              <Category className={`${!curCategory ? 'checked' : ''}`}>
+                <CategoryInput
                   type="checkbox"
-                  value={data.title}
-                  checked={curCategory === data.title}
-                  onChange={() => handleCategoryFilter(data.title)}
+                  value="전체보기"
+                  checked={!curCategory}
+                  onChange={() => viewAll()}
                 />
-                {data.title}
-              </label>
+                전체보기
+              </Category>
+            </CategoryLabel>
+            {categoryData.map(data => (
+              <CategoryLabel>
+                <Category
+                  key={data.id}
+                  className={`${curCategory === data.title ? 'checked' : ''}`}
+                >
+                  <CategoryInput
+                    type="checkbox"
+                    value={data.title}
+                    checked={curCategory === data.title}
+                    onChange={() => handleCategoryFilter(data.title)}
+                  />
+                  {data.title}
+                </Category>
+              </CategoryLabel>
             ))}
-          </div>
+          </CategoryFilter>
         </div>
       </Filter>
     </aside>
