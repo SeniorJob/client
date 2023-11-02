5932eab8e2a3427f768b40049ccb894b50fc9bcb
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-03 00:20:43 +0900

    :sparkles: Status store 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -4,16 +4,20 @@ interface SearchState {
   inputValue: string;
   searchQuery: string | null;
   category: string;
+  status: string;
   setInputValue: (value: string) => void;
   setSearchQuery: (query: string) => void;
   setCategory: (category: string) => void;
+  setStatus: (statis: string) => void;
 }
 
 export const useSearchStore = create<SearchState>(set => ({
   inputValue: '',
   searchQuery: '',
   category: '',
-  setCategory: category => set({ category: category }),
+  status: '',
   setInputValue: value => set({ inputValue: value }),
   setSearchQuery: query => set({ searchQuery: query }),
+  setCategory: category => set({ category: category }),
+  setStatus: status => set({ status: status }),
 }));
