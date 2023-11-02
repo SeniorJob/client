cfbcdb1472b6a1f2359a7686301689afd0ee63e5
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-02 17:41:03 +0900

    :sparkles: Search store 함수 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,14 +2,18 @@ import { create } from 'zustand';
 
 interface SearchState {
   inputValue: string;
-  searchQuery: string;
+  searchQuery: string | null;
+  category: string;
   setInputValue: (value: string) => void;
   setSearchQuery: (query: string) => void;
+  setCategory: (category: string) => void;
 }
 
 export const useSearchStore = create<SearchState>(set => ({
   inputValue: '',
   searchQuery: '',
+  category: '',
+  setCategory: category => set({ category: category }),
   setInputValue: value => set({ inputValue: value }),
   setSearchQuery: query => set({ searchQuery: query }),
 }));
