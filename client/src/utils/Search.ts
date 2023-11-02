f35d497e381c58849d9ad600a9bfac31eace6147
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-03 00:21:15 +0900

    :sparkles: Parameter에 status 추가

--- client/src/utils/Search.ts
+++ client/src/utils/Search.ts
@@ -3,16 +3,19 @@ import { NavigateFunction } from 'react-router-dom';
 type ParamProps = {
   title?: string;
   category?: string;
+  status?: string;
 };
 
 export const SearchSubmitHandler = (
   navigate: NavigateFunction,
   query?: string,
   category?: string,
+  status?: string,
 ) => {
   const paramsObj: ParamProps = {};
   if (query) paramsObj.title = query;
   if (category) paramsObj.category = category;
+  if (status) paramsObj.status = status;
 
   const searchParams = new URLSearchParams(paramsObj);
   navigate({
