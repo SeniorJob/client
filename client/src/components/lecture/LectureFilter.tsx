d4ff56eb4d58985aec016ea697a380ade94b74dc
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-03 00:22:10 +0900

    :sparkles: LectureFilter 컴포넌트 작업

--- client/src/components/lecture/LectureFilter.tsx
+++ client/src/components/lecture/LectureFilter.tsx
@@ -1,3 +1,85 @@
+import styled from 'styled-components';
+import { Tag } from '../../assets/styles/CommonStyles';
+import { statusData } from './statusData';
+import FilterSVG from '../../assets/images/filter.svg?react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useSearchStore } from '../../store/store';
+import { RegionSearchBar } from './RegionSerachBar';
+
+const StatusFilter = styled.div`
+  display: flex;
+  height: max-content;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.4rem;
+`;
+
+const StatusTag = styled(Tag)`
+  padding: 0.4rem 0.7rem;
+  font-weight: unset;
+  border: 1px solid #ccc;
+`;
+
+const StatusLabel = styled.label`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: 0.9rem;
+  gap: 0.4rem;
+  .checked {
+    border-color: var(--primaryColor);
+    color: var(--primaryColor);
+    font-weight: 500;
+    &::before {
+      content: '✓';
+      margin-right: 0.2rem;
+    }
+  }
+`;
+
+const StatusInput = styled.input`
+  display: none;
+`;
+
 export const LectureFilter = () => {
-  return <div>강의 필터 컨테이너</div>;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { setStatus } = useSearchStore();
+  const searchParams = new URLSearchParams(location.search);
+  const curStatus = searchParams.get('status');
+  const handleStatusFilter = (status: string) => {
+    if (curStatus === status) {
+      setStatus('');
+      searchParams.delete('status');
+    } else {
+      setStatus(status);
+      searchParams.set('status', status);
+    }
+    navigate({
+      pathname: '/lectures/filter',
+      search: searchParams.toString(),
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <RegionSearchBar />
+      <StatusFilter>
+        <FilterSVG width={24} height={24} />
+        {statusData.map(status => (
+          <StatusLabel key={status.id}>
+            <StatusTag className={curStatus === status.name ? 'checked' : ''}>
+              <StatusInput
+                type="checkbox"
+                value={status.name}
+                checked={curStatus === status.name}
+                onChange={() => handleStatusFilter(status.name)}
+              />
+              {status.name}
+            </StatusTag>
+          </StatusLabel>
+        ))}
+      </StatusFilter>
+    </div>
+  );
 };
