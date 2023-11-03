ef58c1f8fbeab3b5b54fd6a7dcecc2ee4a11ebf8
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-03 23:44:56 +0900

    :sparkles: Method, descending 함수 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -6,11 +6,15 @@ interface SearchState {
   category: string;
   status: string;
   region: string;
+  filterMethod: string; // 추가: 정렬 방법 (latest, popular, priceHigh, priceLow)
+  descending: boolean; // 추가: 정렬 순서 (true: 내림차순, false: 오름차순)
   setInputValue: (value: string) => void;
   setSearchQuery: (query: string) => void;
   setCategory: (category: string) => void;
-  setStatus: (statis: string) => void;
+  setStatus: (status: string) => void;
   setRegion: (region: string) => void;
+  setFilterMethod: (method: string) => void; // 추가: 정렬 방법 설정
+  setDescending: (desc: boolean) => void; // 추가: 정렬 순서 설정
 }
 
 export const useSearchStore = create<SearchState>(set => ({
@@ -19,9 +23,13 @@ export const useSearchStore = create<SearchState>(set => ({
   category: '',
   status: '',
   region: '',
+  filterMethod: 'latest', // 기본 정렬 방법 설정
+  descending: true, // 기본 정렬 순서 설정
   setInputValue: value => set({ inputValue: value }),
   setSearchQuery: query => set({ searchQuery: query }),
   setCategory: category => set({ category: category }),
   setStatus: status => set({ status: status }),
   setRegion: region => set({ region: region }),
+  setFilterMethod: method => set({ filterMethod: method }), // 정렬 방법 설정
+  setDescending: desc => set({ descending: desc }), // 정렬 순서 설정
 }));
