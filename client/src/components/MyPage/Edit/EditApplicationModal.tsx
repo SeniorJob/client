540463f9ddff5b4f7c38f677dc2e4bb8e1356f9c
Limttugi <junhuk820@gmail.com>
2023-11-14 13:34:25 +0900

    ♻️ 신청한 강좌 수정 리팩토링

---
+++ client/src/components/MyPage/Edit/EditApplicationModal.tsx
@@ -0,0 +1,134 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import {
+  deleteLecture,
+  getDetailOfApplyLectures,
+  updateApplyReason,
+} from '../../../api/mypage';
+
+const EditApplicationModal = ({
+  createId,
+  leId,
+  setShowModal,
+}: {
+  createId?: number;
+  leId?: number;
+  setShowModal: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const [reason, setReason] = useState('');
+  const handleCloseModal = () => setShowModal(false);
+
+  const handleUpdateApplyReason = async () => {
+    if (createId) {
+      await updateApplyReason({
+        lectureId: createId,
+        newApplyReason: reason,
+      });
+    }
+
+    handleCloseModal();
+  };
+
+  const handleDeleteLecture = async () => {
+    await deleteLecture({ type: '신청', id: leId });
+
+    handleCloseModal();
+    location.reload();
+  };
+
+  useEffect(() => {
+    const handleGetDetailOfApplyLectures = async () => {
+      if (createId) {
+        const res = await getDetailOfApplyLectures(createId);
+        console.log(res);
+        res.status === 200 && setReason(res.data.lectureApplyDto.applyReason);
+      }
+    };
+
+    handleGetDetailOfApplyLectures();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Label>
+          신청이유
+          <TextArea value={reason} onChange={e => setReason(e.target.value)} />
+        </Label>
+        <ButtonContainer>
+          <Button onClick={handleUpdateApplyReason}>이유수정하기</Button>
+          <Button onClick={handleDeleteLecture}>신청철회하기</Button>
+        </ButtonContainer>
+        <Button onClick={handleCloseModal}>창닫기</Button>
+      </Container>
+      <ModalBackground onClick={handleCloseModal}></ModalBackground>
+    </>
+  );
+};
+
+export default EditApplicationModal;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  width: 400px;
+  height: 380px;
+  z-index: 101;
+  border-radius: 10px;
+  padding: 30px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const TextArea = styled.textarea`
+  border: 1px solid lightgray;
+  padding: 10px;
+  border-radius: 10px;
+  resize: none;
+  height: 180px;
+  margin-top: 4px;
+  outline: none;
+  &:focus {
+    border-color: green;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 10px;
+  margin-top: 10px;
+`;
+const Button = styled.button.attrs({ type: 'button' })`
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  height: 44px;
+  width: 100%;
+  &:hover {
+    border-color: green;
+  }
+`;
+
+const ModalBackground = styled.div`
+  border: 1px solid black;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 100;
+  cursor: pointer;
+`;
