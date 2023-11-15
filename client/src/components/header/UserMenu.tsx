5db0e006f733860f9f4b3085bd6a92687ceeccc2
nuyhv <129922828+nuyhv@users.noreply.github.com>
2023-11-15 12:34:28 +0900

    :recycle: 드롭다운 컴포넌트 개선

--- client/src/components/header/UserMenu.tsx
+++ client/src/components/header/UserMenu.tsx
@@ -4,15 +4,11 @@ import {
   StyledLoginUserMenu,
 } from '../../assets/styles/MenuStyle';
 import LoginComponent from '../login/loginForm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import defaultImage from '../../assets/images/imageDefault.png';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import {
-  useLoginUserModalStore,
-  useLoginModalStore,
-  useUserStore,
-} from '../../store/user';
+import { useLoginModalStore, useUserStore } from '../../store/user';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
@@ -24,15 +20,11 @@ const MenuList = styled.div`
 axios.defaults.withCredentials = true;
 
 export const UserMenu: React.FC = () => {
-  // zustand로 Modal을 관리하게 되어서 주석 처리
-  // const [isModal, setIsModal] = useState(false);
   const { loginModal, handleLoginModal } = useLoginModalStore();
-  const { loginUserModal, handleLoginUserModal } = useLoginUserModalStore();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { userDetail } = useUserStore();
-  // const [isUserModal, setIsUserModal] = useState(false);
   const [userInfo, setUserInfo] = useState('');
   const setIsLoggedIn = useUserStore().setIsLoggedIn;
-  // const isLoggedIn = useUserStore().isLoggedIn;
   const accessToken = localStorage.getItem('accessToken');
   const LoginInfo = localStorage.getItem('isLogIn');
   const navigate = useNavigate();
@@ -41,9 +33,28 @@ export const UserMenu: React.FC = () => {
     handleLoginModal();
   };
 
-  const handleUserModal = () => {
-    handleLoginUserModal();
+  const handleDropdown = () => {
+    setIsMenuOpen(prev => !prev);
   };
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  // 드롭다운 외부클릭 함수
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   useEffect(() => {
     axios
@@ -103,22 +114,17 @@ export const UserMenu: React.FC = () => {
           </Link>
         </>
       ) : (
-        <>
+        <div className="flex gap-2 relative" ref={dropdownRef}>
           <img
             className="w-8 h-8 rounded-[20px] hover:cursor-pointer mt-1"
             src={userDetail.imgKey ? userDetail.imgKey : defaultImage}
-            onClick={handleUserModal}
+            onClick={handleDropdown}
           />
           <StyledLoginUser>
             <strong className="">{userDetail.name}님</strong> 어서오세요
           </StyledLoginUser>
-        </>
-      )}
-      {/* 이 아래 부분 모달 컴포넌트로 따로 빼는게 가독성이 좋습니다. 안빼도 상관은 없는데 코드 위치가 로그인 - 회원가입 사이에 있는 것은 가독성이 떨어집니다. 위치 옮겼습니다 */}
-      {loginUserModal ? (
-        <>
-          <UserModalBackdrop onClick={handleUserModal}>
-            <UserModalView>
+          {isMenuOpen && (
+            <UserDropdownView>
               <Link to={'/mypage'}>
                 <StyledLoginUserMenu>마이페이지</StyledLoginUserMenu>
               </Link>
@@ -130,10 +136,11 @@ export const UserMenu: React.FC = () => {
               >
                 로그아웃
               </StyledLoginUserMenu>
-            </UserModalView>
-          </UserModalBackdrop>
-        </>
-      ) : null}
+            </UserDropdownView>
+          )}
+        </div>
+      )}
+      {/* 이 아래 부분 모달 컴포넌트로 따로 빼는게 가독성이 좋습니다. 안빼도 상관은 없는데 코드 위치가 로그인 - 회원가입 사이에 있는 것은 가독성이 떨어집니다. 위치 옮겼습니다 */}
       {loginModal ? (
         // 모달에 백그라운드 주는것도 CSS 가상요소 써서 만드는 방법도 있습니다. 그렇게되면 div 하나가 빠지겠죠
         <ModalBackdrop onClick={handleLoginFormModal}>
@@ -174,33 +181,17 @@ const ModalView = styled.div`
   background-color: #ffffff;
 `;
 
-const UserModalBackdrop = styled.div`
-  // Modal이 떴을 때의 배경을 깔아주는 CSS를 구현
-  z-index: 1; //위치지정 요소
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-const UserModalView = styled.div`
-  // Modal창 CSS를 구현합니다.
+const UserDropdownView = styled.div`
   border: solid 2px #1dc078;
   z-index: 3;
   width: 100px;
   height: 80px;
-  position: fixed;
+  position: absolute;
   display: flex;
   flex-direction: column; // 컨텐츠를 세로 방향으로 정렬
   align-items: center;
   justify-content: center; // 수평, 수직 중앙 정렬
-  border-radius: 20px;
+  border-radius: 0.7rem;
   background-color: #ffffff;
-  top: 6%; /* 적절한 값으로 조정하세요. */
-  right: 27.4%; /* 적절한 값으로 조정하세요. */
+  top: 42px; /* 적절한 값으로 조정하세요. */
 `;
